330161506e91a60c5a9fd18c08ef7f14a8bee8a5
Alfred <alfredscyriac@gmail.com>
2026-04-26 10:36:52 -0400

    update ui on mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 flex items-center px-4 sm:px-6 py-4">
       <Link href="/">
-        <Image src="/queenslingo.png" alt="QueensLingo" width={160} height={160} className="h-18 w-auto" />
+        <Image src="/queenslingo.png" alt="QueensLingo" width={160} height={160} className="h-12 w-auto" />
       </Link>
     </nav>
   );
